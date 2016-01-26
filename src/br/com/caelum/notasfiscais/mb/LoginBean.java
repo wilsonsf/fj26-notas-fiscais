6dad0bd95de4642368dbd1c0e8ad7d4cc9fcfc0f
Wilson de Farias <wilsonsf1@gmail.com>
2016-01-26 15:47:27 -0200

    8.9 Gerenciando escopo das dependências (Login/usuario)
    	modified:   WebContent/produto.xhtml
    	modified:   src/br/com/caelum/notasfiscais/mb/LoginBean.java
    	new file:   src/br/com/caelum/notasfiscais/mb/UsuarioLogadoBean.java

--- src/br/com/caelum/notasfiscais/mb/LoginBean.java
+++ src/br/com/caelum/notasfiscais/mb/LoginBean.java
@@ -2,31 +2,34 @@ package br.com.caelum.notasfiscais.mb;
 
 import java.io.Serializable;
 
-import javax.enterprise.context.SessionScoped;
-//import javax.faces.bean.SessionScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.caelum.notasfiscais.dao.UsuarioDao;
 import br.com.caelum.notasfiscais.modelo.Usuario;
 
-@SessionScoped
-@Named//@ManagedBean
+@Named
+@RequestScoped
 public class LoginBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Inject
 	private UsuarioDao dao;
+	@Inject
+	private UsuarioLogadoBean usuarioLogado;
 
 	private Usuario usuario = new Usuario();
 
 	public String efetuaLogin() {
 
 		boolean loginValido = this.dao.existe(this.usuario);
-		if (loginValido)
+		if (loginValido) {
+			this.usuarioLogado.logar(this.usuario);
 			return "produto?faces-redirect=true";
-		else {
+		} else {
+			this.usuarioLogado.deslogar();
 			limpaFormulario();
 			return null;
 		}
