0ad23585fe1cedc34cd2fc5ea3dc686122a9e87e
Wilson de Farias <wilsonsf1@gmail.com>
2016-01-27 12:02:40 -0200

    10.4 Formulario da Nota fiscal, adição de itens e listagem (Detail)
    	modified:   WebContent/notafiscal.xhtml
    	modified:   src/br/com/caelum/notasfiscais/mb/NotaFiscalBean.java

--- src/br/com/caelum/notasfiscais/mb/NotaFiscalBean.java
+++ src/br/com/caelum/notasfiscais/mb/NotaFiscalBean.java
@@ -5,15 +5,23 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.caelum.notasfiscais.dao.NotaFiscalDao;
+import br.com.caelum.notasfiscais.dao.ProdutoDao;
+import br.com.caelum.notasfiscais.modelo.Item;
 import br.com.caelum.notasfiscais.modelo.NotaFiscal;
+import br.com.caelum.notasfiscais.modelo.Produto;
 
 @Named
+//@ManagedBean
 @RequestScoped
 public class NotaFiscalBean {
 
 	@Inject
 	private NotaFiscalDao notaFiscalDao;
+	@Inject
+	private ProdutoDao produtoDao;
 
+	private Long idProduto;
+	private Item item = new Item();
 	private NotaFiscal notaFiscal = new NotaFiscal();
 
 	public String grava() {
@@ -24,8 +32,38 @@ public class NotaFiscalBean {
 		return null;
 	}
 
+	public void guardaItem() {
+		Produto produto = this.produtoDao.buscaPorId(this.idProduto);
+
+		this.item.setProduto(produto);
+		this.item.setValorUnitario(produto.getPreco());
+
+		this.notaFiscal.getItens().add(this.item);
+		this.item.setNotaFiscal(this.notaFiscal);
+
+		this.item = new Item();
+		this.idProduto = null;
+
+	}
+
 	public NotaFiscal getNotaFiscal() {
 		return this.notaFiscal;
+	}
+
+	public Long getIdProduto() {
+		return this.idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public Item getItem() {
+		return this.item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
 	}
 
 }
